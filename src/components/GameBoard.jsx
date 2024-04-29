870c7fa655bb1f1bf89ac19b96ee7f95e64405f5
Ahmmed-Hassan <ahmednhoo@gmail.com>
2024-04-29 06:39:18 +0300

    finished

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
 
-export default function GameBoard({onSquareSelect, turns}) {
-let gameBoard = initialGameBoard;
 
-for(const turn of turns){
-  const {square, player} = turn;
-  const {row, col} = square;
+export default function GameBoard({onSquareSelect, board}) {
 
-  gameBoard[row][col] = player;
-}
 
     // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
@@ -27,12 +16,12 @@ for(const turn of turns){
     // }
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={()=> onSquareSelect(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button onClick={()=> onSquareSelect(rowIndex, colIndex)} disabled={playerSymbol !== null}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
